7dcec4e0f0b53ea8b12322504661777c1fb4e339
11129 <gerald.bartoli@vtsd.com>
2022-01-24 10:05:50 -0500

    javascript

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,9 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
+function task1button(){
+	var button = document.getElementById("buttonTask1");
+	button.style.display = "block";
+}
+function showimagetask1(){
+	var showImage = document.getElementById("task1img2");
+	showImage.style.display = "block";
 }
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,13 @@
 /* Put code for the task below */
+function buttoncolor1() {
+    var table = document.getElementById("color1");
+
+}
+function buttoncolor2() {
+    var table = document.getElementById("color2");
+
+}
+function buttoncolor3() {
+    var table = document.getElementById("color3");
+
+}
